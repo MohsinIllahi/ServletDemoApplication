d4b11a8c95e602d44d27b8f6f18cf27a4a7156d5
Mohsin illahi <mbhutto424@gmail.com>
2026-02-01 15:50:05 +0500

    new dev - added sessionManagement, cookies and sendRedirect demo in servlet

--- src/main/java/com/millahi/AddServlet.java
+++ src/main/java/com/millahi/AddServlet.java
@@ -6,9 +6,11 @@ import java.io.PrintWriter;
 import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
 
 //@WebServlet("/add")
 public class AddServlet extends HttpServlet
@@ -20,12 +22,23 @@ public class AddServlet extends HttpServlet
 		
 		int k = i + j;
 		
+		//session management can also be used to pass parameter to a servlet
+//		HttpSession session = req.getSession();
+//		session.setAttribute("k", k);
+		
+		//
+		Cookie cookie = new Cookie("k", k+"");
+		res.addCookie(cookie);
+		
 		PrintWriter out = res.getWriter();
 		
 //		out.println("result is " + k);
 		// we can send data from one servlet to another by using setAttribute where there will be key and vaue like a map 
-		req.setAttribute("k", k);
-		RequestDispatcher rDispatcher = req.getRequestDispatcher("sq");
-		rDispatcher.forward(req, res);
+//		req.setAttribute("k", k);
+//		RequestDispatcher rDispatcher = req.getRequestDispatcher("sq");
+//		rDispatcher.forward(req, res);
+		
+		//sendRedirect example
+		res.sendRedirect("sq");
 	}
 }
--- src/main/java/com/millahi/SqServlet.java
+++ src/main/java/com/millahi/SqServlet.java
@@ -4,17 +4,45 @@ import java.io.IOException;
 import java.io.PrintWriter;
 
 import jakarta.servlet.ServletException;
+import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
 
 public class SqServlet extends HttpServlet 
 {
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		// we can get the attribute which is passed from other servlet in request by using .getAttribute(nameOfKey) method
-		int k = (int) req.getAttribute("k");
+		int k =0;
+		// We can retrieve attributes passed from another servlet using request.getAttribute("key").
+		//
+		// When working within the same application, RequestDispatcher.forward() is preferred
+		// because it forwards the request internally without changing the URL and
+		// preserves the same request and response objects.
+		//
+		// If redirection is required, response.sendRedirect() is used.
+		// It changes the URL and creates a new request, so request attributes are not preserved.
+		// This is commonly used for external systems like payment gateways or CAPTCHA services.
+
+//		 k = (int) req.getAttribute("k");
 		PrintWriter out= resp.getWriter();
-		out.println(k*k);
+//		out.println("square of addition is "+k*k);
+		
+		//sendRedirect example
+		//session management
+//		HttpSession session = req.getSession();
+//		 k = (int) session.getAttribute("k");
+		
+		//cookie 
+		Cookie[] cookies = req.getCookies();
+		for (Cookie cookie : cookies) {
+			if (cookie.getName().equals("k")) {
+				k= Integer.parseInt(cookie.getValue());
+			}
+		}
+//		 k = Integer.parseInt(req.getParameter("k"));
+		k=k*k;
+		out.print("Result is " +k);
 	}
 }
